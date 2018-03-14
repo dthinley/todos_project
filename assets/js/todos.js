d2953a21743a0ef0de7dec70690798ff3e431685
Dorjee Thinlay <dthinley@yahoo.com>
2018-03-13 19:23:56 -0700

    Add files via upload

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -19,7 +19,9 @@ $("input[type='text']").keypress(function(event){
 		$(this).val("");
 		//create a new li and add to ul
 		$("ul").append("<li><span><i class = 'fa fa-trash'></i></span> " + todoText + "</li>")
-
 	}
-
 });
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();
+})
